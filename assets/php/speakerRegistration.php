a578a15612154547f03762265e925c22b57601ad
Tony Thomas <01tonythomas@gmail.com>
2014-10-05 01:25:16 +0530

    Fixed captcha issues. Now working fine.

--- assets/php/speakerRegistration.php
+++ assets/php/speakerRegistration.php
@@ -38,83 +38,95 @@ EOF;
 $name = $email = $org = $city = $prearrival = $predeparture = $profile = $pretitle = "";
 $lap = $accom = 0;
 $nameerror = $emailerror = $arrivalerror = $departureerror = $orgerror = $cityerror = $titleerror = $profilerror = "";
-if ( $_SERVER["REQUEST_METHOD"] == "POST" )
-{
-	$myDateTime = new DateTime( Date( '' ), new DateTimeZone( 'GMT' ) );
-	$myDateTime->setTimezone( new DateTimeZone( 'Asia/Kolkata' ) );
-	$date = $myDateTime->format( 'Y-m-d H:i:s' );
-	$name = $_POST['sp-name'];
-	if ( empty( $_POST['sp-email'] ) )
-	{
-		$emailerror = "Required Field";
-	}
-	else
-	{
-		$email = $_POST['sp-email'];
-		if ( !preg_match( "/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email ) )
+if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+	require_once( 'recaptchalib.php' );
+	$privatekey = "key_replaced_in_server";
+	$resp = recaptcha_check_answer ( $privatekey,
+	$_SERVER["REMOTE_ADDR"],
+	$_POST["recaptcha_challenge_field"],
+	$_POST["recaptcha_response_field"] );
+
+	if ( !$resp->is_valid ) {
+		// What happens when the CAPTCHA was entered incorrectly
+		die ( "The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+		"(reCAPTCHA said: " . $resp->error . ")" );
+	} else {
+		$myDateTime = new DateTime( Date( '' ), new DateTimeZone( 'GMT' ) );
+		$myDateTime->setTimezone( new DateTimeZone( 'Asia/Kolkata' ) );
+		$date = $myDateTime->format( 'Y-m-d H:i:s' );
+		$name = $_POST['sp-name'];
+		if ( empty( $_POST['sp-email'] ) )
 		{
-			$emailerror = "Invalid Format";
+			$emailerror = "Required Field";
 		}
-	}
-	$org = $_POST['sp-org'];
-	$city = $_POST['sp-city'];
-	if ( !preg_match( '/$^|^[a-zA-Z]+[0-9]*[\. ,]*[a-zA-Z0-9]*$/', $city ) )
-	{
-		$cityerror = "City name must start with a letter and can contain only alphanumerics, spaces, periods and commas";
-	}
-	if ( empty( $_POST['sp-profile'] ) ) {
-		$profilerror = "No profile";
-	} else {
-		$profile = $_POST['sp-profile'];
-	}
-
-	if ( empty( $_POST['sp-tshirt'] ) ) {
-		$tshirt = "0";
-	} else {
-		$tshirt = $_POST['sp-tshirt'];
-	}
-
-	if ( empty( $_POST['sp-arrival'] ) ) {
-		$arrivalerror = "No arriving date given";
-	} else {
-		$arrival = $_POST['sp-arrival'];
-	}
-	if ( empty( $_POST['sp-depart'] ) ) {
-		$departureerror = "No departure date given";
-	} else {
-		$departure = $_POST['sp-depart'];
-	}
-	$lap = 1;
-	if ( empty( $_POST['sp-accom'] ) ) {
-		$accom = "0";
-	} else {
-		$accom = $_POST['sp-accom'];
-	}
-	$pretitle = $_POST['sp-title'];
-	if ( empty( $pretitle ) )
-	{
-		$titleerror = "Required Field";
-	}
-	else
-	{
-		$title = $_POST['sp-title'];
-		$desc = $_POST['sp-desc'];
-
-	}
-	if ( $nameerror == "" && $emailerror == "" && $arrivalerror == "" && $departureerror == "" && $orgerror == "" && $cityerror == "" && $titleerror == "" && $profilerror == "" )
-	{
-		$sql = "INSERT INTO `registration_speaker` VALUES ('$name','$email','$org','$city','$lap','$accom','$tshirt','$arrival','$departure','$title','$desc','$date', '$profile')";
-
-		$ret = $db->exec( $sql );
-		if ( $ret )
+		else
 		{
-			$db->close();
-			header( 'location:../../registration_success.html' );
+			$email = $_POST['sp-email'];
+			if ( !preg_match( "/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email ) )
+			{
+				$emailerror = "Invalid Format";
+			}
+		}
+		$org = $_POST['sp-org'];
+		$city = $_POST['sp-city'];
+		if ( !preg_match( '/$^|^[a-zA-Z]+[0-9]*[\. ,]*[a-zA-Z0-9]*$/', $city ) )
+		{
+			$cityerror = "City name must start with a letter and can contain only alphanumerics, spaces, periods and commas";
+		}
+		if ( empty( $_POST['sp-profile'] ) ) {
+			$profilerror = "No profile";
+		} else {
+			$profile = $_POST['sp-profile'];
+		}
+
+		if ( empty( $_POST['sp-tshirt'] ) ) {
+			$tshirt = "0";
+		} else {
+			$tshirt = $_POST['sp-tshirt'];
+		}
+
+		if ( empty( $_POST['sp-arrival'] ) ) {
+			$arrivalerror = "No arriving date given";
+		} else {
+			$arrival = $_POST['sp-arrival'];
+		}
+		if ( empty( $_POST['sp-depart'] ) ) {
+			$departureerror = "No departure date given";
+		} else {
+			$departure = $_POST['sp-depart'];
+		}
+		$lap = 1;
+		if ( empty( $_POST['sp-accom'] ) ) {
+			$accom = "0";
+		} else {
+			$accom = $_POST['sp-accom'];
+		}
+		$pretitle = $_POST['sp-title'];
+		if ( empty( $pretitle ) )
+		{
+			$titleerror = "Required Field";
+		}
+		else
+		{
+			$title = $_POST['sp-title'];
+			$desc = $_POST['sp-desc'];
+
+		}
+		if ( $nameerror == "" && $emailerror == "" && $arrivalerror == "" && $departureerror == "" && $orgerror == "" && $cityerror == "" && $titleerror == "" && $profilerror == "" )
+		{
+			$sql = "INSERT INTO `registration_speaker` VALUES ('$name','$email','$org','$city','$lap','$accom','$tshirt','$arrival','$departure','$title','$desc','$date', '$profile')";
+
+			$ret = $db->exec( $sql );
+			if ( $ret )
+			{
+				$db->close();
+				header( 'location:../../registration_success.html' );
+			} else {
+				header( 'location:../../registration_fail.html' );
+			}
 		} else {
 			header( 'location:../../registration_fail.html' );
 		}
-	} else {
-		header( 'location:../../registration_fail.html' );
 	}
 }
 ?>
