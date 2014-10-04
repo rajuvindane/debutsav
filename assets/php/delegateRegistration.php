4e4946f6536be2fd541b7b7f4dc7aa148e10f786
Tony Thomas <01tonythomas@gmail.com>
2014-10-05 01:03:19 +0530

    Added missing fields in registration page

--- assets/php/delegateRegistration.php
+++ assets/php/delegateRegistration.php
@@ -1,28 +1,28 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+error_reporting( E_ALL );
+ini_set( 'display_errors', '1' );
 /**
  * Created by PhpStorm.
  * User: router
  * Date: 10/3/14
  * Time: 1:20 PM
  */
-#Database Connection
+# Database Connection
 class database extends SQLite3
 {
 	function __construct()
 	{
-		$this->open('minidebconf.db');
+		$this->open( 'minidebconf.db' );
 	}
 }
 $db = new database();
-if (!$db)
+if ( !$db )
 {
 	echo $db->lastErrorMsg();
 }
 else
 {
-	$sql =<<<EOF
+	$sql = <<<EOF
     CREATE TABLE IF NOT EXISTS registration
     (NAME          TEXT    NOT NULL,
     EMAIL         TEXT    NOT NULL,
@@ -35,64 +35,75 @@ else
     DEPARTURE     TEXT,
     REGTIME       TEXT);
 EOF;
-	$ret = $db->exec($sql);
+	$ret = $db->exec( $sql );
 }
 
-$name=$email=$org=$city=$prearrival=$predeparture=$arrival=$departure="";
-$lap=$accom=0;
+$name = $email = $org = $city = $prearrival = $predeparture = $arrival = $departure = "";
+$lap = $accom = 0;
 $nameerror = $emailerror = $arrivalerror = $departureerror = $orgerror = $cityerror = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$myDateTime = new DateTime(Date(''), new DateTimeZone('GMT'));
-	$myDateTime->setTimezone(new DateTimeZone('Asia/Kolkata'));
-	$date=$myDateTime->format('Y-m-d H:i:s');
-	$name= $_POST['del-name'];
-	if (empty($_POST['del-email']))
+if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+	$myDateTime = new DateTime( Date( '' ), new DateTimeZone( 'GMT' ) );
+	$myDateTime->setTimezone( new DateTimeZone( 'Asia/Kolkata' ) );
+	$date = $myDateTime->format( 'Y-m-d H:i:s' );
+	$name = $_POST['del-name'];
+	if ( empty( $_POST['del-email'] ) )
 	{
 		$emailerror = "Required Field";
 	}
 	else
 	{
 		$email = $_POST['del-email'];
-		if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+		if ( !preg_match( "/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email ) )
 		{
 			$emailerror = "Invalid Format";
 		}
 	}
 	$org = $_POST['del-org'];
 	$city = $_POST['del-city'];
-	if(!preg_match('/$^|^[a-zA-Z]+[0-9]*[\. ,]*[a-zA-Z0-9]*$/',$city))
+	if ( !preg_match( '/$^|^[a-zA-Z]+[0-9]*[\. ,]*[a-zA-Z0-9]*$/', $city ) )
 	{
 		$cityerror = "City name must start with a letter and can contain only alphanumerics, spaces, periods and commas";
 	}
 
-	if( empty( $_POST['del-arrival'] ) ) {
+	if ( empty( $_POST['del-arrival'] ) ) {
 
-		$arrivalerror= "No arriving date given";
+		$arrivalerror = "No arriving date given";
 	} else {
 
 		$arrival = $_POST['del-arrival'];
 	}
-	if( empty( $_POST['del-depart'] ) ) {
-		$departureerror= "No departure date given";
+	if ( empty( $_POST['del-depart'] ) ) {
+		$departureerror = "No departure date given";
 	} else {
 		$departure = $_POST['del-depart'];
 	}
-	$lap=$accom=$tshirt=1;
-	if ($nameerror=="" && $emailerror=="" && $arrivalerror=="" && $departureerror=="" && $orgerror =="" && $cityerror=="")
+	$lap = 1;
+	if ( empty( $_POST['del-accom'] ) ) {
+		$accom = "0";
+	} else {
+		$accom = $_POST['del-accom'];
+	}
+	if ( empty( $_POST['del-tshirt'] ) ) {
+		$tshirt = "0";
+	} else {
+		$tshirt = $_POST['del-tshirt'];
+	}
+
+	if ( $nameerror == "" && $emailerror == "" && $arrivalerror == "" && $departureerror == "" && $orgerror == "" && $cityerror == "" )
 	{
 
-		$sql="INSERT INTO `registration` VALUES ('$name','$email','$org','$city','$lap','$accom','$tshirt','$arrival','$departure','$date')";
+		$sql = "INSERT INTO `registration` VALUES ('$name','$email','$org','$city','$lap','$accom','$tshirt','$arrival','$departure','$date')";
 
-		$ret = $db->exec($sql);
-		if($ret)
+		$ret = $db->exec( $sql );
+		if ( $ret )
 		{
 			$db->close();
-			header('location:../../registration_success.html');
+			header( 'location:../../registration_success.html' );
 		} else {
-			header('location:../../registration_fail.html');
+			header( 'location:../../registration_fail.html' );
 		}
 	} else {
-		header('location:../../registration_fail.html');
+		header( 'location:../../registration_fail.html' );
 	}
 }
 ?>
--- assets/php/speakerRegistration.php
+++ assets/php/speakerRegistration.php
@@ -1,22 +1,22 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-#Database Connection
+error_reporting( E_ALL );
+ini_set( 'display_errors', '1' );
+# Database Connection
 class database extends SQLite3
 {
 	function __construct()
 	{
-		$this->open('minidebconf.db');
+		$this->open( 'minidebconf.db' );
 	}
 }
 $db = new database();
-if (!$db)
+if ( !$db )
 {
 	echo $db->lastErrorMsg();
 }
 else
 {
-	$sql =<<<EOF
+	$sql = <<<EOF
     CREATE TABLE IF NOT EXISTS registration_speaker
     (NAME          TEXT    NOT NULL,
     EMAIL         TEXT    NOT NULL,
@@ -32,80 +32,89 @@ else
     REGTIME       TEXT,
     PROFILE	  TEXT);
 EOF;
-	$ret = $db->exec($sql);
+	$ret = $db->exec( $sql );
 
 }
-$name=$email=$org=$city=$prearrival=$predeparture=$profile=$pretitle="";
-$lap=$accom=0;
-$nameerror = $emailerror = $arrivalerror = $departureerror = $orgerror = $cityerror = $titleerror = $profilerror ="";
-if ($_SERVER["REQUEST_METHOD"] == "POST")
+$name = $email = $org = $city = $prearrival = $predeparture = $profile = $pretitle = "";
+$lap = $accom = 0;
+$nameerror = $emailerror = $arrivalerror = $departureerror = $orgerror = $cityerror = $titleerror = $profilerror = "";
+if ( $_SERVER["REQUEST_METHOD"] == "POST" )
 {
-	$myDateTime = new DateTime(Date(''), new DateTimeZone('GMT'));
-	$myDateTime->setTimezone(new DateTimeZone('Asia/Kolkata'));
-	$date=$myDateTime->format('Y-m-d H:i:s');
+	$myDateTime = new DateTime( Date( '' ), new DateTimeZone( 'GMT' ) );
+	$myDateTime->setTimezone( new DateTimeZone( 'Asia/Kolkata' ) );
+	$date = $myDateTime->format( 'Y-m-d H:i:s' );
 	$name = $_POST['sp-name'];
-	if (empty($_POST['sp-email']))
+	if ( empty( $_POST['sp-email'] ) )
 	{
 		$emailerror = "Required Field";
 	}
 	else
 	{
 		$email = $_POST['sp-email'];
-		if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+		if ( !preg_match( "/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email ) )
 		{
 			$emailerror = "Invalid Format";
 		}
 	}
 	$org = $_POST['sp-org'];
 	$city = $_POST['sp-city'];
-	if(!preg_match('/$^|^[a-zA-Z]+[0-9]*[\. ,]*[a-zA-Z0-9]*$/',$city))
+	if ( !preg_match( '/$^|^[a-zA-Z]+[0-9]*[\. ,]*[a-zA-Z0-9]*$/', $city ) )
 	{
 		$cityerror = "City name must start with a letter and can contain only alphanumerics, spaces, periods and commas";
 	}
-	if( empty( $_POST['sp-profile'] ) ) {
+	if ( empty( $_POST['sp-profile'] ) ) {
 		$profilerror = "No profile";
 	} else {
 		$profile = $_POST['sp-profile'];
 	}
 
-	$tshirt = 1; //Needs to be fixed
+	if ( empty( $_POST['sp-tshirt'] ) ) {
+		$tshirt = "0";
+	} else {
+		$tshirt = $_POST['sp-tshirt'];
+	}
 
-	if( empty( $_POST['sp-arrival'] ) ) {
-		$arrivalerror= "No arriving date given";
+	if ( empty( $_POST['sp-arrival'] ) ) {
+		$arrivalerror = "No arriving date given";
 	} else {
 		$arrival = $_POST['sp-arrival'];
 	}
-	if( empty( $_POST['sp-depart'] ) ) {
-		$departureerror= "No departure date given";
+	if ( empty( $_POST['sp-depart'] ) ) {
+		$departureerror = "No departure date given";
 	} else {
 		$departure = $_POST['sp-depart'];
 	}
-	$lap = $accom = 1; //Needs to be fixed
-	$pretitle=$_POST['sp-title'];
-	if (empty($pretitle))
+	$lap = 1;
+	if ( empty( $_POST['sp-accom'] ) ) {
+		$accom = "0";
+	} else {
+		$accom = $_POST['sp-accom'];
+	}
+	$pretitle = $_POST['sp-title'];
+	if ( empty( $pretitle ) )
 	{
 		$titleerror = "Required Field";
 	}
 	else
 	{
-		$title=$_POST['sp-title'];
+		$title = $_POST['sp-title'];
 		$desc = $_POST['sp-desc'];
 
 	}
-	if ($nameerror=="" && $emailerror=="" && $arrivalerror=="" && $departureerror=="" && $orgerror =="" && $cityerror=="" && $titleerror == "" && $profilerror == "" )
+	if ( $nameerror == "" && $emailerror == "" && $arrivalerror == "" && $departureerror == "" && $orgerror == "" && $cityerror == "" && $titleerror == "" && $profilerror == "" )
 	{
-		$sql="INSERT INTO `registration_speaker` VALUES ('$name','$email','$org','$city','$lap','$accom','$tshirt','$arrival','$departure','$title','$desc','$date', '$profile')";
+		$sql = "INSERT INTO `registration_speaker` VALUES ('$name','$email','$org','$city','$lap','$accom','$tshirt','$arrival','$departure','$title','$desc','$date', '$profile')";
 
-		$ret = $db->exec($sql);
-		if($ret)
+		$ret = $db->exec( $sql );
+		if ( $ret )
 		{
 			$db->close();
-			header('location:../../registration_success.html');
+			header( 'location:../../registration_success.html' );
 		} else {
-			header('location:../../registration_fail.html');
+			header( 'location:../../registration_fail.html' );
 		}
 	} else {
-		header('location:../../registration_fail.html');
+		header( 'location:../../registration_fail.html' );
 	}
 }
 ?>
